005de13cd43f393fa6098a75fdbb224d824088bc
Nikita Kovalev <kovalnick434@gmail.com>
2022-03-29 10:10:16 +0300

    Added descriptions in docstrings, refactored names, fixed dict indexation

--- Materials/data_processing/mafa_reformat.py
+++ Materials/data_processing/mafa_reformat.py
@@ -21,29 +21,29 @@ def read_mat_file(folder, filepath):
     return scipy.io.loadmat(filename)
 
 
-def mat_to_files(mat, is_test: bool):
+def mafa_mat_to_coco_files(mat: dict, is_test: bool) -> None:
     """
-
-    :param mat:
-    :param is_test:
-    :return:
+    Convert every line in MAFA data mat to the YOLO training format (text files with normalised coordinates of the face)
+    :param mat: input MATLAB object of certain structure (enable debug mode to inspect closer)
+    :param is_test: set to True if the test data is given, False if train
     """
 
     for row in mat:
-        im_path = row['imgName'][0][0]
-        coco_row = datarow_to_coco(row['label'][0][0], is_test, image_shape=
+        im_path = row['name'][0][0] if is_test else row['imgName'][0][0]
+        coco_row = mafa_datarow_to_coco(row['label'][0][0], is_test, image_shape=
             imread(os.path.join(PATH, images_test_path if is_test else images_train_path, im_path)).shape[0:2])
 
         with open(os.path.join(PATH, labels_test_save_folder if is_test else labels_train_save_folder, im_path.split('.')[0]), "a") as f:
             f.writelines([coco_row])
 
 
-def datarow_to_coco(line, is_test: bool, image_shape: Tuple[int, int]):
+def mafa_datarow_to_coco(line: np.ndarray, is_test: bool, image_shape: Tuple[int, int]) -> str:
     """
-
-    :param line:
-    :param is_test:
-    :return:
+    Convert a single row of MAFA mat data to YOLO suitable line for one face
+    :param line: one MAFA datarow
+    :param is_test: set to True if the test data is given, False if train
+    :param image_shape: shape of the image for which the data is being transformed
+    :return: YOLO-formatted string for respective MAFA datarow
     """
     x, y, w, h = line[:4]
 
@@ -75,8 +75,8 @@ if __name__ == "__main__":
     train_mat = read_mat_file(PATH, labels_train_path)['label_train'].transpose((1, 0))
     test_mat = read_mat_file(PATH, labels_test_path)['LabelTest'].transpose((1, 0))
 
-    # os.makedirs(os.path.join(PATH, labels_train_save_folder))
-    # os.makedirs(os.path.join(PATH, labels_test_save_folder))
+    os.makedirs(os.path.join(PATH, labels_train_save_folder))
+    os.makedirs(os.path.join(PATH, labels_test_save_folder))
 
-    mat_to_files(train_mat, is_test=False)
-    mat_to_files(test_mat, is_test=True)
+    mafa_mat_to_coco_files(train_mat, is_test=False)
+    mafa_mat_to_coco_files(test_mat, is_test=True)
